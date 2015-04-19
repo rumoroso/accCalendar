a6c94ed5c3e83257021390831819ed28498f6d80
Jorge Rumoroso <jorge.rumoroso@mimacom.com>
2015-04-19 19:07:16 +0200

    update first example

---
+++ scripts/app.js
@@ -0,0 +1,41 @@
+'use strict';
+
+// Initialize Angular Application
+var myApp = angular
+    .module('exampleNgAccCalendar', [
+        'ngAccCalendar'
+    ])
+    .controller('exampleController', function ($scope, calendarModelService) {
+        $scope.configuration = {
+            initialDate: new Date(2015, 3, 18),
+            yearRange: 5,
+            //disableCurrentDate: false,
+            //disableWeekends: true,
+            disabledDates: {
+                exceptionDates: [
+                    new Date(2015, 3, 1),
+                    new Date(2016, 4, 1),
+                    new Date(2015, 3, 24),
+                    new Date(2015, 3, 8),
+                    new Date(2015, 3, 9),
+                    new Date(2015, 3, 16),
+                    new Date(2015, 3, 10),
+                    new Date(2015, 4, 11),
+                    new Date(2015, 4, 13),
+                    new Date(2015, 4, 14)],
+                regularDates: [
+                    {month: 2, day: 18},
+                    {month: 2, day: 26},
+                    {month: 3, day: 1},
+                    {month: 4, day: 14},
+                    {month: 4, day: 15}
+                ]
+            },
+            minDate: new Date(2015, 3, 10),
+            maxDate: new Date(2016, 4, 10),
+            setDefaultDate: true,
+            format: 'd m yyyy'
+            //showWeekNumber: true,
+            //visible: true
+        };
+    });
